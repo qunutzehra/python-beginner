6d9b758506297c3a6be9595287e42570dc605d35
qunutzehra <57519999+qunutzehra@users.noreply.github.com>
2022-12-11 00:53:04 +0530

    Add files via upload

---
+++ addDigitFromTwoDigitNumber.py.py
@@ -0,0 +1,13 @@
+# Take two digit number from user
+twoDigitNumber=input("Enter two digit number: ")
+
+# add two digit number together 
+
+first=twoDigitNumber[0]
+second=twoDigitNumber[1]
+
+print("First digit is: ",first)
+print("Second digit is: ",second)
+
+num=int(first) + int(second)
+print(num)
