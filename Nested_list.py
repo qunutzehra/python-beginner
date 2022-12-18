0903146346079405c52e6902b3ab401742bce111
qunutzehra <57519999+qunutzehra@users.noreply.github.com>
2022-12-18 20:49:13 +0530

    Add files via upload

---
+++ Nested_list.py
@@ -0,0 +1,8 @@
+# create a nested list where we can store different type of data in single list
+
+fruits = ['Apple', 'Banana', 'Blueberry', 'Jackfruit', 'Kiwi', 'Mango', 'Oranges', 'Watermelon', 'Muskmelon' ]
+vegetables = ['Tomato', 'Patato', 'Cabbage', 'Carrot', 'Onion', 'Mushroom', 'Pumpkin', 'Broccoli', 'Green Peas']
+fruits_vegetable = [fruits, vegetables]
+
+
+print(fruits_vegetable)
